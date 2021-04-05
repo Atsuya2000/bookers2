8300960014cc16d1ffbf4c810f387f625be2c5bd
Atsuya2000 <atuyajyun@gmail.com>
2021-04-05 13:45:47 +0000

    FinalCommit

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
     resources :book_comments, only: [:create, :destroy]
   end
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
 
 end
